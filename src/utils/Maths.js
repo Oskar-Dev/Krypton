469838b55b53a9c7d19559b733350d78268f9263
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-07-01 19:10:12 +0200

    optimalizations to isVerticalAsymptote & added evaluteFunction

--- src/utils/Maths.js
+++ src/utils/Maths.js
@@ -6,13 +6,13 @@ export const derivativeAtPoint = (f, x_0, h) => {
   return (f(x_0 + h) - f(x_0)) / h;
 };
 
-export const isVerticalAsymptote = (f, h, x_1, x_2) => {
+export const isVerticalAsymptote = (derivativeAtX_0, derivativeAtX_1, valueAtX_0, valueAtX_1) => {
   return (
-    (Math.sign(derivativeAtPoint(f, x_1, h)) > 0 && Math.sign(derivativeAtPoint(f, x_2, h)) > 0 && f(x_2) < f(x_1)) ||
-    (Math.sign(derivativeAtPoint(f, x_1, h)) < 0 && Math.sign(derivativeAtPoint(f, x_2, h)) < 0 && f(x_2) > f(x_1)) ||
-    (Math.sign(derivativeAtPoint(f, x_1, h)) == 0 && Math.sign(derivativeAtPoint(f, x_2, h)) == 0 && f(x_2) != f(x_1)) ||
-    !isFinite(f(x_1)) ||
-    !isFinite(f(x_2))
+    (Math.sign(derivativeAtX_0) > 0 && Math.sign(derivativeAtX_1) > 0 && valueAtX_1 < valueAtX_0) ||
+    (Math.sign(derivativeAtX_0) < 0 && Math.sign(derivativeAtX_1) < 0 && valueAtX_1 > valueAtX_0) ||
+    (Math.sign(derivativeAtX_0) == 0 && Math.sign(derivativeAtX_1) == 0 && valueAtX_1 != valueAtX_0) ||
+    !isFinite(valueAtX_0) ||
+    !isFinite(valueAtX_1)
   );
 };
 
@@ -54,5 +54,41 @@ export const findVerticalAsymptote = (f, h, n, a, b) => {
   }
 
   data.value = (a + b) / 2;
+  return data;
+};
+
+export const evaluateFunction = (f, from, to, delta) => {
+  const h = 0.0000001;
+  const maxDiff = 0.005;
+  const minDiff = 20;
+  var data = [];
+
+  for (var x = from / delta; x <= to / delta; x++) {
+    var x_ = x * delta;
+    data.push({ argument: x_, value: f(x_) });
+  }
+
+  // var size = data.length;
+  // for (var i = 1; i < size - 1; i++) {
+  //   var previous = data[i - 1];
+  //   var current = data[i];
+  //   var next = data[i + 1];
+
+  //   var derivativeAtPrevious = derivativeAtPoint(f, previous.value, h);
+  //   var derivativeAtCurrent = derivativeAtPoint(f, current.value, h);
+  //   var derivativeAtNext = derivativeAtPoint(f, next.value, h);
+
+  //   if (
+  //     (Math.abs(derivativeAtPrevious - derivativeAtCurrent) <= maxDiff &&
+  //       Math.abs(derivativeAtCurrent - derivativeAtNext) <= maxDiff) ||
+  //     (Math.abs(derivativeAtPrevious - derivativeAtCurrent) >= minDiff &&
+  //       Math.abs(derivativeAtCurrent - derivativeAtNext) >= minDiff)
+  //   ) {
+  //     data.splice(i, 1);
+  //     i--;
+  //     size = data.length;
+  //   }
+  // }
+
   return data;
 };
