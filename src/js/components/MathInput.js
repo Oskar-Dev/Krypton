288fef815b2f4fcb2e20e1bc5bb0bea86a897f15
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-07-05 00:20:40 +0200

    made it look little better

--- src/js/components/MathInput.js
+++ src/js/components/MathInput.js
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
 import './MathInput.scss';
+import TeX from '@matejmazur/react-katex';
+import 'katex/dist/katex.min.css';
 
 const MathInput = ({ callback }) => {
   return (
     <div className='inputContainer'>
-      <input onChange={(e) => callback(e.target.value)}></input>
+      <button
+        className='deleteButton'
+        onClickCapture={(e) => e.target.blur()}
+        // onDrag={(e) => e.target.blur()}
+        tabindex='-1'
+      ></button>
+      <input
+        spellCheck='false'
+        onChange={(e) => {
+          var value = e.target.value;
+          callback(value);
+        }}
+      ></input>
+      <TeX className='math' style={{ color: 'white' }}>
+        {'f(x)='}
+      </TeX>
       <div className='box'></div>
     </div>
   );
