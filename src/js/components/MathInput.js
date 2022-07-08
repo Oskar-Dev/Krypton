5893c222003f950fba07e62c1640600d764bde24
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-07-08 12:59:38 +0200

    added settings button

--- src/js/components/MathInput.js
+++ src/js/components/MathInput.js
@@ -1,28 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './MathInput.scss';
 import TeX from '@matejmazur/react-katex';
 import 'katex/dist/katex.min.css';
+import { ReplaceList } from '../../utils/FunctionReplaceList';
+import { replaceAll } from '../../utils/replaceAll';
+import { BsGearFill } from 'react-icons/bs';
+import { MdClear } from 'react-icons/md';
 
 const MathInput = ({ callback }) => {
+  const openSettings = () => {
+    console.log('settings button');
+  };
+
+  const handleDelete = () => {
+    console.log('delete button');
+  };
+
   return (
     <div className='inputContainer'>
-      <button
-        className='deleteButton'
-        onClickCapture={(e) => e.target.blur()}
-        // onDrag={(e) => e.target.blur()}
-        tabindex='-1'
-      ></button>
+      <div className='deleteButton buttonWrapper'>
+        <MdClear className='button' size={36} onClick={() => handleDelete()} />
+      </div>
       <input
         spellCheck='false'
         onChange={(e) => {
           var value = e.target.value;
+          var keys = Object.keys(ReplaceList);
+
+          keys.forEach((key) => {
+            value = replaceAll(value, key, ReplaceList[key]);
+          });
+
           callback(value);
         }}
       ></input>
       <TeX className='math' style={{ color: 'white' }}>
         {'f(x)='}
       </TeX>
-      <div className='box'></div>
+      <div className='box settingsButton'>
+        <BsGearFill className='button' size={24} onClick={() => openSettings()} />
+      </div>
     </div>
   );
 };
