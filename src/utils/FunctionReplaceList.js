e9055d40fc4f2d5ea1de2c5344ba426a7cbcdbb9
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-07-08 12:59:53 +0200

    Create FunctionReplaceList.js

---
+++ src/utils/FunctionReplaceList.js
@@ -0,0 +1,11 @@
+export const ReplaceList = {
+  log: 'log10',
+  ln: 'log',
+  ctg: 'cot',
+  tg: 'tan',
+  cosec: 'csc',
+  arccos: 'acos',
+  arcsin: 'asin',
+  arctg: 'atan',
+  arcctg: 'acot',
+};
