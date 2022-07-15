e4179463aacacdc7c23a58207535f7c376c6fd25
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-07-15 15:13:12 +0200

    settings

--- src/js/components/GraphSettings.js
+++ src/js/components/GraphSettings.js
@@ -1,41 +1,72 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './GraphSettings.scss';
-import { MdClear } from 'react-icons/md';
+import { MdOpacity } from 'react-icons/md';
+import { BsBorderWidth } from 'react-icons/bs';
+import { toGraph } from '../../utils/toGraph';
 
-const GraphSettings = ({ callback }) => {
+const GraphSettings = ({ blurCallback, index }) => {
   const containerRef = useRef(null);
+  const { settings } = toGraph[index];
+  // const { color, opacity, width } = settings;
+  const [opacity, setOpacity] = useState(settings.opacity);
+  const [width, setWidth] = useState(settings.width);
+  const [color, setColor] = useState(settings.color);
+
+  const handleBlur = (event) => {
+    // if the blur was because of outside focus
+    // currentTarget is the parent element, relatedTarget is the clicked element
+    if (!event.currentTarget.contains(event.relatedTarget)) {
+      blurCallback();
+    }
+  };
+
+  const updateValue = (valueName, value) => {
+    toGraph[index].settings[valueName] = value;
+  };
 
   useEffect(() => {
     containerRef.current.focus();
   }, []);
 
   return (
-    <div className='graphSettingsContainer' ref={containerRef} tabIndex={0} onBlur={() => callback()}>
-      {/* <div className='titleContainer'>
-        <p className='titleText'>Ustawienia Wykresu</p>
-
-        <div className='icon closeIcon'>
-          <MdClear size={32} />
-        </div>
+    <div className='graphSettingsContainer' ref={containerRef} tabIndex={0} onBlur={(event) => handleBlur(event)}>
+      <div className='arrow'>
+        <div className='innerArrow'></div>
       </div>
 
-      <div className='settingsContainer'>
-        <div className='settingWrapper'>
-          <p className='settingName'>Kolor</p>
-          <div className='color'></div>
+      <div className='inlineWrapper'>
+        <div className='icon'>
+          <MdOpacity size={20} />
         </div>
 
-        <div className='settingWrapper'>
-          <p className='settingName'>Przezroczystość</p>
-          <div className='color'></div>
+        <input
+          className='input'
+          value={opacity}
+          onChange={(e) => {
+            var value = e.target.value;
+
+            updateValue('opacity', value);
+            setOpacity(value);
+          }}
+        />
+      </div>
+
+      <div className='inlineWrapper'>
+        <div className='icon'>
+          <BsBorderWidth size={20} />
         </div>
 
-        <div className='settingWrapper'>
-          <p className='settingName'>Grubość</p>
-          <div className='color'></div>
-        </div>
-      </div> */}
-      <p>test</p>
+        <input
+          className='input'
+          value={width}
+          onChange={(e) => {
+            var value = e.target.value;
+
+            updateValue('width', value);
+            setWidth(value);
+          }}
+        />
+      </div>
     </div>
   );
 };
