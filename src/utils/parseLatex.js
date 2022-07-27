7815ed7a9c17da5eb558cfe0f24d8f0dbf761625
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-07-27 23:21:51 +0200

    Create parseLatex.js
    convers latex string to string that can be evaluated by matjs

---
+++ src/utils/parseLatex.js
@@ -0,0 +1,74 @@
+import { replaceAll } from './replaceAll.js';
+
+const CLOSE_BRACKET_AT = ['\\', '+', '-', '*', '{', '}'];
+const DONT_OPEN_BRACKET_AT = ['\\', '(', '{'];
+
+export const parseLatex = (latex) => {
+  latex = replaceAll(latex, '\\frac', '');
+  latex = replaceAll(latex, '\\cdot', '*');
+  latex = replaceAll(latex, 'operatorname{abs}', '\\abs');
+
+  // set math constants
+  latex = replaceAll(latex, '\\pi', '(pi)');
+  latex = replaceAll(latex, '\\phi', '(phi)');
+
+  // idk if this works
+  latex = replaceAll(latex, '\\left|', '\\abs(');
+  latex = replaceAll(latex, '\\right|', ')');
+
+  // replace the cosmetic things
+  latex = replaceAll(latex, '\\left', '');
+  latex = replaceAll(latex, '\\right', '');
+
+  var openedBracket = false;
+  for (var i = 0; i < latex.length; i++) {
+    var char = latex[i];
+
+    if (openedBracket && CLOSE_BRACKET_AT.includes(char)) {
+      latex = latex.slice(0, i) + ')' + latex.slice(i);
+      openedBracket = false;
+      i++;
+    }
+
+    if (char === '\\' && i < latex.length - 1) {
+      i++;
+      char = latex[i];
+      var functionName = '\\';
+
+      while (char.match(/[a-z]/g) && i < latex.length) {
+        functionName += char;
+
+        i++;
+        char = latex[i];
+      }
+
+      if (!openedBracket && latex[i + 1] !== '\\' && !DONT_OPEN_BRACKET_AT.includes(latex[i])) {
+        latex = latex.slice(0, i) + '(' + latex.slice(i);
+        openedBracket = true;
+        i++;
+      }
+
+      switch (functionName) {
+        case '\\log':
+          latex = latex.replace(functionName, 'log10');
+          break;
+
+        case '\\ln':
+          latex = latex.replace(functionName, 'log');
+          break;
+
+        default:
+          continue;
+      }
+    }
+  }
+
+  if (openedBracket) latex += ')';
+
+  latex = replaceAll(latex, '}{', ')/(');
+  latex = replaceAll(latex, '}', ')');
+  latex = replaceAll(latex, '{', '(');
+  latex = replaceAll(latex, '\\', '');
+
+  return latex;
+};
