069da2f7f4c2c87905da9f7c1d573504ba7d3f58
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-07-27 23:21:07 +0200

    now using mathjs evaluate func

--- src/js/graphFunctions/evalutePoints.js
+++ src/js/graphFunctions/evalutePoints.js
@@ -3,9 +3,14 @@ import { isComplex } from 'mathjs';
 const evaluatePoints = (f, from, to, delta) => {
   var data = [];
 
+  // console.log(latex);
+  // var l = parseLatex(latex);
+  // console.log(l);
+
   for (var x = Math.floor(from / delta); x <= Math.floor(to / delta); x++) {
     var x_ = x * delta;
-    var value = f({ x: x_ });
+    // var value = f({ x: x_ });
+    var value = f.evaluate({ x: x_ });
 
     if (isComplex(value)) value = undefined;
 
