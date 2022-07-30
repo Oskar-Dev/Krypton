1c6fedb10bf11528e1c86a198432c6331adf9ff3
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-07-31 00:28:01 +0200

    added asymptote check

--- src/js/graphFunctions/evalutePoints.js
+++ src/js/graphFunctions/evalutePoints.js
@@ -1,4 +1,5 @@
 import { isComplex } from 'mathjs';
+import { derivativeAtPoint, isVerticalAsymptote } from '../../utils/Maths';
 
 const evaluatePoints = (f, from, to, delta) => {
   const MAX_VALUE = 2 ** 28;
@@ -9,13 +10,21 @@ const evaluatePoints = (f, from, to, delta) => {
   // console.log(l);
 
   for (var x = Math.floor(from / delta); x <= Math.floor(to / delta); x++) {
-    var x_ = x * delta;
+    var argument = x * delta;
     // var value = f({ x: x_ });
-    var value = f.evaluate({ x: x_ });
+    var value = f.evaluate({ x: argument });
 
     if (isComplex(value) || isNaN(value) || value >= MAX_VALUE || value <= -MAX_VALUE) continue;
+    var pointData = { x: argument, y: value, hole: false };
 
-    var pointData = { x: x_, y: value };
+    if (data.length > 0) {
+      var prevPoint = data[data.length - 1];
+
+      var prevSlope = derivativeAtPoint(f, prevPoint.x, 1e-10);
+      var currentSlope = derivativeAtPoint(f, argument, 1e-10);
+
+      if (isVerticalAsymptote(prevSlope, currentSlope, prevPoint.y, value)) pointData.hole = true;
+    }
 
     data.push(pointData);
   }
