cb02893501805cf0453a81ec48ed6a066f9bea7d
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-08-03 15:20:31 +0200

    now removes lonely points & fixed limits

--- src/js/graphFunctions/evalutePoints.js
+++ src/js/graphFunctions/evalutePoints.js
@@ -1,11 +1,11 @@
-import { isComplex } from 'mathjs';
+import math, { e, isComplex } from 'mathjs';
 import { derivativeAtPoint, findHole, findOneWayAsymptote, limit } from '../../utils/Maths';
 
 const evaluatePoints = (f, from, to, delta) => {
   const MAX_VALUE = 2 ** 28;
   const h = 1e-8;
   const deltaX = 1e-5;
-  const limitDelta = 1e-8;
+  const limitDelta = 1e-7;
   const data = [];
 
   for (var x = Math.floor(from / delta); x <= Math.floor(to / delta); x++) {
@@ -21,6 +21,14 @@ const evaluatePoints = (f, from, to, delta) => {
       var prevSlope = prevPoint.slope;
       var prevValue = prevPoint.y;
       var prevArgument = prevPoint.x;
+
+      // check for sigle points
+      if (data.length > 1) {
+        var prevPrevPoint = data[data.length - 2];
+
+        if (Math.abs(prevPrevPoint.x - prevArgument) > delta * 2 && Math.abs(prevArgument - argument) > delta * 2)
+          data.pop();
+      }
 
       if (Math.sign(prevSlope) === 1 && Math.sign(slope) === 1 && prevValue > value) {
         pointData.hole = true;
@@ -61,26 +69,35 @@ const evaluatePoints = (f, from, to, delta) => {
           pointData.holeX = argument - delta / 2;
         }
       } else if (Math.sign(prevSlope) === -1 && Math.sign(slope) === 1) {
-        var prevArgument0 = prevArgument + deltaX;
-        var argument0 = argument - deltaX;
-
-        var holeX_ = findHole(f, prevArgument, argument);
-        var limLeft = limit(f, holeX_, limitDelta, -1);
-        var limRight = limit(f, holeX_, limitDelta, 1);
-
-        if (
-          derivativeAtPoint(f, prevArgument, h) > derivativeAtPoint(f, prevArgument0, h) &&
-          derivativeAtPoint(f, argument0, h) > derivativeAtPoint(f, argument, h) &&
-          limLeft === '-infinity' &&
-          limRight === '-infinity'
-        ) {
-          pointData.hole = true;
-
+        if (Math.abs(prevArgument - argument) > delta * 2) {
           prevPoint.lim = -1;
           pointData.lim = -1;
 
-          prevPoint.holeX = argument + delta / 2;
+          prevPoint.holeX = prevPoint.x + delta / 2;
           pointData.holeX = argument - delta / 2;
+          pointData.hole = true;
+        } else {
+          var prevArgument0 = prevArgument + deltaX;
+          var argument0 = argument - deltaX;
+
+          var holeX_ = findHole(f, prevArgument, argument);
+          var limLeft = limit(f, holeX_, limitDelta, -1);
+          var limRight = limit(f, holeX_, limitDelta, 1);
+
+          if (
+            derivativeAtPoint(f, prevArgument, h) > derivativeAtPoint(f, prevArgument0, h) &&
+            derivativeAtPoint(f, argument0, h) > derivativeAtPoint(f, argument, h) &&
+            limLeft === '-infinity' &&
+            limRight === '-infinity'
+          ) {
+            pointData.hole = true;
+
+            prevPoint.lim = -1;
+            pointData.lim = -1;
+
+            prevPoint.holeX = argument + delta / 2;
+            pointData.holeX = argument - delta / 2;
+          }
         }
       }
     }
@@ -88,10 +105,17 @@ const evaluatePoints = (f, from, to, delta) => {
     data.push(pointData);
   }
 
-  // check first and last point limits
   var firstPoint = data[0];
+  var secondPoint = data[1];
   var lastPoint = data[data.length - 1];
+  var secondLastPoint = data[data.length - 2];
 
+  // check if first or last point are lonely
+  if (Math.abs(firstPoint.x - secondPoint.x) > delta * 2) data.shift();
+
+  if (Math.abs(lastPoint.x - secondLastPoint.x) > delta * 2) data.pop();
+
+  // check first and last point limits
   var firstX = findOneWayAsymptote(f, firstPoint.x - delta, firstPoint.x);
   var lastX = findOneWayAsymptote(f, lastPoint.x, lastPoint.x + delta);
 
