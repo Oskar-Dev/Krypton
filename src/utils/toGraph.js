4c6cffbdad4dd684635d3ca760befe3fd4d45f45
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-08-03 23:27:48 +0200

    added lineDash setting

--- src/utils/toGraph.js
+++ src/utils/toGraph.js
@@ -4,7 +4,7 @@ export const defaultGraphSettings = {
   opacity: 0.9,
   width: 3,
   color: graphColors[0],
-  lineDash: { style: [], index: 0 },
+  lineDash: 0,
 };
 
 export const lineDashStyles = [[], [10], [1, 10]];
