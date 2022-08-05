98ec0e0d2ecb2c3ca7343470079552b9a8db990f
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-08-05 20:30:14 +0200

    added point style & adjusted size

--- src/js/graphFunctions/renderPoints.js
+++ src/js/graphFunctions/renderPoints.js
@@ -1,16 +1,24 @@
-const renderPoints = (canvasContext, centerX, centerY, points, scale) => {
+const renderPoints = (canvasContext, centerX, centerY, points, scale, size, style) => {
   if (points.length < 1 || points === undefined) return;
 
   canvasContext.beginPath();
+
+  var radius = style === 0 ? size : size;
 
   for (var i = 0, n = points.length; i < n; i++) {
     var point = points[i];
     var { x, y } = point;
 
-    canvasContext.arc(centerX + x * scale, centerY - y * scale, 5, 0, Math.PI * 2);
+    canvasContext.arc(centerX + x * scale, centerY - y * scale, radius, 0, Math.PI * 2);
   }
 
+  canvasContext.lineWidth = size * 2;
   canvasContext.stroke();
+
+  if (style === 1) {
+    canvasContext.fillStyle = '#181717';
+    canvasContext.fill();
+  }
 };
 
 export default renderPoints;
