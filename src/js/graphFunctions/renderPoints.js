bf4e0a79479462b1914c4cca3b8a134ace07cb79
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-08-07 14:37:37 +0200

    don't connect points

--- src/js/graphFunctions/renderPoints.js
+++ src/js/graphFunctions/renderPoints.js
@@ -1,19 +1,18 @@
 const renderPoints = (canvasContext, centerX, centerY, points, scale, size, style) => {
   if (points.length < 1 || points === undefined) return;
 
-  canvasContext.beginPath();
-
   var radius = style === 0 ? size : size;
+  canvasContext.lineWidth = size * 2;
 
   for (var i = 0, n = points.length; i < n; i++) {
     var point = points[i];
     var { x, y } = point;
 
+    // canvasContext.moveTo(centerX + x * scale, centerY - y * scale);
+    canvasContext.beginPath();
     canvasContext.arc(centerX + x * scale, centerY - y * scale, radius, 0, Math.PI * 2);
+    canvasContext.stroke();
   }
-
-  canvasContext.lineWidth = size * 2;
-  canvasContext.stroke();
 
   if (style === 1) {
     canvasContext.fillStyle = '#181717';
