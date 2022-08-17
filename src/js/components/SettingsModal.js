a37e1b1ca1e0168ece623485edae4a034b231910
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-08-18 01:07:34 +0200

    Create SettingsModal.js

---
+++ src/js/components/SettingsModal.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import '../../scss/SettingsModal.scss';
+
+const SettingsModal = ({ open, handleClose }) => {
+  const handleSettingPageChange = (clickedElement) => {
+    const elements = document.getElementsByClassName('settingPageButton');
+
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      element.classList.remove('selectedSettingPage');
+    }
+
+    clickedElement.classList.add('selectedSettingPage');
+  };
+
+  if (!open) return null;
+
+  return ReactDom.createPortal(
+    <>
+      <div className='modalOverlay' onClick={handleClose} />
+      <div className='modalContentWrapper'>
+        <div className='settingsModalLeft'>
+          <div className='settingsModalTopLeft'>
+            <p>Ustawienia</p>
+          </div>
+
+          <div className='settingsModalBottomLeft'>
+            <div
+              className='settingPageButton selectedSettingPage'
+              onClick={(event) => handleSettingPageChange(event.target)}
+            >
+              <p>Ogólne</p>
+            </div>
+
+            <div className='settingPageButton' onClick={(event) => handleSettingPageChange(event.target)}>
+              <p>Wygląd</p>
+            </div>
+
+            <div className='settingPageButton' onClick={(event) => handleSettingPageChange(event.target)}>
+              <p>Osie</p>
+            </div>
+
+            <div className='settingPageButton' onClick={(event) => handleSettingPageChange(event.target)}>
+              <p>Siatka</p>
+            </div>
+
+            <div className='settingPageButton' onClick={(event) => handleSettingPageChange(event.target)}>
+              <p>Wykres</p>
+            </div>
+          </div>
+        </div>
+        <div className='settingsModalRight'></div>
+      </div>
+    </>,
+    document.getElementById('portal')
+  );
+};
+
+export default SettingsModal;
