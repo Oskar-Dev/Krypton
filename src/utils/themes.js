ecd8929f2a88e28ac263d51214295b118f038af0
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-08-19 21:30:25 +0200

    added themes

---
+++ src/utils/themes.js
@@ -0,0 +1,50 @@
+const light = {
+  '--color-primary-400': '#ccc',
+  '--color-primary-600': '#fff',
+  '--color-primary-700': '#ededed',
+
+  '--color-accent': '#4da6ff',
+
+  '--color-text': '#0e0f12',
+  '--color-text-darker': '#25282e',
+
+  '--color-primary-600-translucent': '#ffffffbf',
+  '--color-accent-translucent': '#4da6ff1a',
+  '--color-text-translucent': '#0e0f121a',
+};
+
+const dark = {
+  '--color-primary-400': '#353535',
+  '--color-primary-600': '#1d1d1d',
+  '--color-primary-700': '#161616',
+
+  '--color-accent': '#4da6ff',
+
+  '--color-text': '#fff',
+  '--color-text-darker': '#999',
+
+  '--color-primary-600-translucent': '#1d1d1dbf',
+  '--color-accent-translucent': '#4da6ff1a',
+  '--color-text-translucent': '#fffffff1a',
+};
+
+const ocean = {
+  '--color-primary-400': '#232c3b',
+  '--color-primary-600': '#171b29',
+  '--color-primary-700': '#141721',
+
+  '--color-accent': '#6e72eb',
+
+  '--color-text': '#fff',
+  '--color-text-darker': '#999',
+
+  '--color-primary-600-translucent': '#171b29bf',
+  '--color-accent-translucent': '#6e72ebf1a',
+  '--color-text-translucent': '#fffffff1a',
+};
+
+export const themes = {
+  light: light,
+  dark: dark,
+  ocean: ocean,
+};
