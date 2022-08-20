ac3c30b3327a4b6f4f7f640118d4197b87b2b217
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-08-20 20:44:19 +0200

    Create SettingElement.js

---
+++ src/js/components/SettingElement.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Dropdown from './Dropdown';
+
+import '../../styles/Settings.css';
+
+const SettingsElement = ({ label, description, type, data, selectedItem, onChange }) => {
+  const selectElements = {
+    dropdown: <Dropdown data={data} selectedItem={selectedItem} onChange={onChange} />,
+  };
+
+  return (
+    <div className='settingsElementWrapper'>
+      <div>
+        <h3>{label}</h3>
+        <p>{description}</p>
+      </div>
+
+      {selectElements[type]}
+    </div>
+  );
+};
+
+export default SettingsElement;
