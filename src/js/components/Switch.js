648737422beb624996ce4c1fa5d87be0b8d4ef73
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-08-23 15:29:40 +0200

    changed attr name

--- src/js/components/Switch.js
+++ src/js/components/Switch.js
@@ -3,8 +3,8 @@ import '../../styles/Switch.css';
 
 const Switch = ({ checked, onChange }) => {
   return (
-    <div className='switchMain' switchChecked={checked.toString()} onClick={() => onChange(!checked)}>
-      <div className='switchCircle' switchChecked={checked.toString()} />
+    <div className='switchMain' ischecked={checked.toString()} onClick={() => onChange(!checked)}>
+      <div className='switchCircle' ischecked={checked.toString()} />
     </div>
   );
 };
