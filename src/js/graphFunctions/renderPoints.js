bedc3fd75e6cb3d466c74795fdc402d483415fae
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-08-24 20:29:18 +0200

    now using correct colors

--- src/js/graphFunctions/renderPoints.js
+++ src/js/graphFunctions/renderPoints.js
@@ -1,3 +1,6 @@
+import { settings } from '../../utils/globalSettings';
+import { themes } from '../../utils/themes';
+
 const renderPoints = (canvasContext, centerX, centerY, points, scale, size, style, label) => {
   if (points.length < 1 || points === undefined) return;
   centerY -= 16;
@@ -5,6 +8,9 @@ const renderPoints = (canvasContext, centerX, centerY, points, scale, size, styl
   // var radius = style === 0 ? size : size;
   var radius = parseFloat(size);
   canvasContext.lineWidth = size * 2;
+
+  var fillColor = themes[settings.general.theme]['--color-primary-600'];
+  var labelColor = themes[settings.general.theme]['--color-text'];
 
   for (var i = 0, n = points.length; i < n; i++) {
     var point = points[i];
@@ -16,12 +22,12 @@ const renderPoints = (canvasContext, centerX, centerY, points, scale, size, styl
     canvasContext.stroke();
 
     if (style === 1) {
-      canvasContext.fillStyle = '#181717';
+      canvasContext.fillStyle = fillColor;
       canvasContext.fill();
     }
 
     // render labels
-    canvasContext.fillStyle = '#fff';
+    canvasContext.fillStyle = labelColor;
     canvasContext.fillText(label, centerX + x * scale, centerY - y * scale + radius * 2 + 10);
   }
 };
