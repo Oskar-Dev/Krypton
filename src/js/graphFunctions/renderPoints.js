2448a6eccbf0ef083f7b7ea96baec58d2ef5a8c8
Oskar-Dev <62214403+Oskar-Dev@users.noreply.github.com>
2022-09-24 19:32:05 +0200

    added rotating points

--- src/js/graphFunctions/renderPoints.js
+++ src/js/graphFunctions/renderPoints.js
@@ -1,7 +1,7 @@
 import { settings } from '../../utils/globalSettings';
 import { themes } from '../../utils/themes';
 
-const renderPoints = (canvasContext, centerX, centerY, points, scale, size, style, label) => {
+const renderPoints = (canvasContext, centerX, centerY, points, scale, size, style, label, rotateGraph) => {
   if (points.length < 1 || points === undefined) return;
   centerY -= 16;
 
@@ -15,6 +15,8 @@ const renderPoints = (canvasContext, centerX, centerY, points, scale, size, styl
   for (var i = 0, n = points.length; i < n; i++) {
     var point = points[i];
     var { x, y } = point;
+
+    if (rotateGraph) [x, y] = [y, x];
 
     // canvasContext.moveTo(centerX + x * scale, centerY - y * scale);
     canvasContext.beginPath();
